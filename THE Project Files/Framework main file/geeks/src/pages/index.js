62dfea83b545181d4729fd055dee016cc3819dd6
kareem2002-k <120688051+kareem2002-k@users.noreply.github.com>
2023-03-18 23:08:01 +0200

    Finally added geeks
    الحمدالله

---
+++ THE Project Files/Framework main file/geeks/src/pages/index.js
@@ -0,0 +1,46 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from '@next/font/google'
+import styles from '@/styles/Home.module.css'
+import Link from 'next/link'
+import 'bootstrap/dist/css/bootstrap.css'
+
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Geeks Hub </title>
+        <meta name="description" content=" A co-working space" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+      <nav className="navbar navbar-expand-lg bg-white"  >
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">
+      <Image src={"/logo geeks.png"} alt="Geeks Hub" width={100} height={100} />
+    </a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Features</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Pricing</a>
+        </li>
+        <button type="button" class="btn btn-outline-dark">Login</button>
+        
+      </ul>
+    </div>
+  </div>
+</nav>
+    </>
+  )
+}
