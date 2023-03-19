983ae7963e5c9bc26ee10a38ac973bca00b4afd1
kareem2002-k <120688051+kareem2002-k@users.noreply.github.com>
2023-03-19 20:48:27 +0200

    Files of Landing page
    Added the files of landing page
    Managed to make the navigation bar function but not 100% styled

---
+++ THE Project Files/Framework main file/geeks/src/pages/landingpage/contact.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import Navbar from '../components/navbar';
+
+
+function Contact(){
+    return(
+        <div>
+            <Navbar></Navbar>
+            <h1>Contact  us</h1>
+        </div>
+
+    )
+}
+export default Contact;
--- THE Project Files/Framework main file/geeks/src/pages/landingpage/home.js
+++ THE Project Files/Framework main file/geeks/src/pages/landingpage/home.js
@@ -4,22 +4,22 @@ import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
 import Link from 'next/link'
 import 'bootstrap/dist/css/bootstrap.css'
-import Navbar from './components/navbar'
-import Home from './landingpage/home'
+import Navbar from '../components/navbar'
 
 
-const inter = Inter({ subsets: ['latin'] })
-
-export default function index () {
-  return (
-    <>
-    
-      <Head>
+function Home(){
+    return(
+        <div>
+            <Head>
         <title>Geeks Hub </title>
         <meta name="description" content=" A co-working space" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
-      <Home></Home>
-    </>
-  )
+           </Head>
+
+            <Navbar></Navbar>
+            <h1>Home</h1>
+        </div>
+
+    )
 }
+export default Home;
