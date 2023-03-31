4090dde5cbb936e27b9fc5599c32b26650495534
kareem2002-k <120688051+kareem2002-k@users.noreply.github.com>
2023-03-31 02:23:11 +0200

    Settings page for user and admin
    Added the profile setting page in which the user or admin makes a change in his name ,email , password or telephone number

--- THE Project Files/final-version/geeks-v-3/components/Sidebar.js
+++ THE Project Files/final-version/geeks-v-3/components/Sidebar.js
@@ -14,7 +14,7 @@ import { useTheme } from "next-themes";
     <div>
         
 
-<aside id="default-sidebar" className="fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0 justify-items-center" aria-label="Sidebar">
+<aside id="default-sidebar" className="fixed top-0 left-0 z-40 w-64 h-full transition-transform -translate-x-full sm:translate-x-0 justify-items-center" aria-label="Sidebar">
    <div className="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
       <ul className="space-y-2 font-medium">
          <li>
@@ -26,7 +26,7 @@ import { useTheme } from "next-themes";
          <li>
             <a href="#" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700">
                <svg aria-hidden="true" className="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M5 3a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V5a2 2 0 00-2-2H5zM5 11a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V5zM11 13a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z"></path></svg>
-               <span className="flex-1 ml-3 whitespace-nowrap">Kanban</span>
+               <span className="flex-1 ml-3 whitespace-nowrap"></span>
                <span className="inline-flex items-center justify-center px-2 ml-3 text-sm font-medium text-gray-800 bg-gray-200 rounded-full dark:bg-gray-700 dark:text-gray-300">Pro</span>
             </a>
          </li>
