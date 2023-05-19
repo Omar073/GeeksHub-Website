2caaa7d570cee5ff0062958f530e68b067589271
HaoTurnip <connection5bars@gmail.com>
2023-05-19 21:53:50 +0300

    Applied changes to landing page, final

--- Geeks code with firebase/geeks/src/LandingPage.js
+++ Geeks code with firebase/geeks/src/LandingPage.js
@@ -1,83 +1,67 @@
-
-import logo from './logo.svg';
-import './App.css';
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Features from "./components/Features";
-import Pricing from "./components/Pricing";
-import { BrowserRouter} from 'react-router-dom';
-import { Routes, Route } from 'react-router-dom';
-import Login from "./Login";
-import NewUser from './NewUser';
-import UserDashboard from './UserDashboard';
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Features from './components/Features';
+import Pricing from './components/Pricing';
 
 function LandingPage() {
-
     return (
+        <div className="bg-gray-100">
+            <Header />
+            <section className="py-16 px-4 bg-gradient-to-br from-indigo-500 to-purple-600 relative z-10">
+                <div className="max-w-5xl mx-auto">
+                    <div className="flex items-center justify-between">
+                        <img className="h-12" src="../logoalt.png" alt="Logo" />
+                        <div className="text-right">
+                            <Link
+                                to="/signup"
+                                className="px-6 py-3 text-lg font-medium text-white bg-indigo-700 rounded-md shadow-md hover:bg-indigo-800"
+                            >
+                                Get Started
+                            </Link>
+                            <Link
+                                to="/login"
+                                className="px-6 py-3 ml-4 text-lg font-medium text-white transition duration-300 border border-white rounded-md shadow-md hover:bg-white hover:text-indigo-700"
+                            >
+                                Log In
+                            </Link>
+                        </div>
+                    </div>
+                    <div className="mt-8">
+                        <h1 className="text-4xl sm:text-5xl md:text-6xl font-bold leading-tight text-white">
+                            Join the hub of innovation and collaboration at{' '}
+                            <span className=" to-blue-400 text-indigo-300 transition-all duration-300 hover:text-white hover:text-7xl hover">
+                                Geeks Hub
+                            </span>
 
-<div className="bg-white dark:bg-black">
-    
-    
-<Header />
-<section className="relative">
-  <div className="px-4 pt-10 mx-auto max-w-7xl md:pt-16">
-    <div className="w-full pb-5 mx-auto text-center md:w-11/12">
-      <h1 className="mb-3 text-5xl font-bold tracking-tight text-gray-900 dark:text-gray-100 md:text-6xl">
-      Join the hub of innovation and collaboration at
-        <br />
-        
-        <span className="text-transparent bg-clip-text bg-gradient-to-r dark:bg-gradient-to-r from-pink-500 via-purple-500 to-indigo-500 dark:from-pink-500 dark:via-purple-400 dark:to-indigo-500">
-           Geeks Hub
-        </span>
-      </h1>
-      <p className="max-w-xl pt-5 mx-auto text-lg text-gray-600 dark:text-gray-400 md:text-lg">
-      Geeks Hub is a co-working space that caters to tech enthusiasts seeking a collaborative and innovative workspace.
-       It provides all necessary amenities for a productive work environment, including high-speed internet, comfortable workstations,
-        and private meeting rooms.
-      </p>
-      <div className="mt-6 text-center md:ml-6">
-       
-        <br className="sm:hidden" />
-        <a href="signup"  className="inline-flex items-center px-5 py-3 mt-2 ml-0 text-sm font-medium text-gray-700 transition duration-300 border rounded shadow dark:hover:border-gray-500 hover:shadow-md md:ml-2 dark:text-gray-300">
-         
-            <span className="flex justify-center">Get Started</span>
-        
-        </a>
-        <a href="login"  className="inline-flex items-center px-5 py-3 mt-2 ml-0 text-sm font-medium text-gray-700 transition duration-300 border rounded shadow dark:hover:border-gray-500 hover:shadow-md md:ml-2 dark:text-gray-300">
-         
-         <span className="flex justify-center">Get Started</span>
-     
-     </a>
-      </div>
-    </div>
-    <div className="relative w-full py-10 mx-auto text-center md:py-32 md:my-12 md:w-10/12">
-      <div className="relative z-10  justify-center">
-        <a
-          target="_blank"
-          rel="noreferrer"
-          href="https://unsplash.com/photos/e9TrFZZ72DQ"
-        >
-          <img
-            className="transition duration-700 shadow-xl rounded-xl ring-1 ring-black ring-opacity-5 hover:transform hover:scale-105"
-            src="images/defaultNews.jpg"
-            alt="Placeholder Image"
-          />
-        </a>
-      </div>
-      <p className="z-10 my-8 text-sm font-medium text-gray-500">
-      Work smarter, not harder at Geeks Hub
-      </p>
-    </div>
+                        </h1>
+                        <p className="mt-6 max-w-xl mx-auto text-xl text-white">
+                            Geeks Hub is a co-working space that caters to tech enthusiasts seeking a collaborative and innovative workspace. It provides all necessary amenities for a productive work environment, including high-speed internet, comfortable workstations, and private meeting rooms.
+                        </p>
+                    </div>
+                </div>
 
-    
-  </div>
-</section>
-<Features />
-<Pricing />
-<Footer />
-</div>
+                <div className="absolute top-0 left-0 w-full h-full bg-gradient-to-br from-transparent via-transparent to-blue-600 bg-opacity-5 pointer-events-none"></div>
 
-);
+            </section>
+            <section className="relative">
+                <div className="max-w-5xl mx-auto py-16">
+                    <div className="flex items-center justify-center">
+                        <img
+                            className="w-full max-w-7xl shadow-md rounded-lg shadow-md"
+                            src="../place.png"
+                            alt="Workspace"
+                        />
+                    </div>
+                    <p className="mt-8 text-xl text-center text-gray-600">Work smarter, not harder at Geeks Hub</p>
+                </div>
+            </section>
+            <Features />
+            <Pricing />
+            <Footer />
+        </div>
+    );
 }
 
 export default LandingPage;
--- Geeks code with firebase/geeks/src/components/Pricing.js
+++ Geeks code with firebase/geeks/src/components/Pricing.js
@@ -1,137 +1,132 @@
-const Feature = () => {
+const Feature = ({ description }) => {
   return (
-    <li className="flex mb-1 space-x-3">
-      <svg
-        width="24"
-        height="24"
-        fill="none"
-        stroke="currentColor"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        className="text-green-500 w-7"
-        aria-hidden="true"
-      >
-        <path d="M20 6L9 17l-5-5" />
-      </svg>
-      <span className="text-sm text-gray-500 dark:text-gray-400">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit
-      </span>
-    </li>
+      <li className="flex mb-1 space-x-3">
+        <svg
+            width="24"
+            height="24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className="text-indigo-500 w-7"
+            aria-hidden="true"
+        >
+          <path d="M20 6L9 17l-5-5" />
+        </svg>
+        <span className="text-sm text-gray-500 dark:text-gray-400">{description}</span>
+      </li>
   );
 };
 
 export default function Pricing() {
   return (
-    <section id="pricing" className="py-12">
-      <div className="max-w-xl px-4 py-12 mx-auto sm:px-6 lg:max-w-6xl lg:px-8">
-        <h1 className="mb-8 text-2xl font-bold tracking-normal text-center text-gray-800 md:leading-tight md:tracking-normal dark:text-gray-200 md:text-4xl">
-          All with{" "}
-          <span className="text-transparent bg-clip-text bg-gradient-to-r dark:bg-gradient-to-r from-indigo-500 via-fuchsia-500 to-rose-500 dark:from-indigo-400 dark:via-fuchsia-400 dark:to-rose-400">
+      <section id="pricing" className="py-12">
+        <div className="max-w-xl px-4 py-12 mx-auto sm:px-6 lg:max-w-6xl lg:px-8">
+          <h1 className="mb-8 text-2xl font-bold tracking-normal text-center text-gray-800 md:leading-tight md:tracking-normal dark:text-gray-200 md:text-4xl">
+            All with{" "}
+            <span className="text-transparent bg-clip-text bg-gradient-to-r dark:bg-gradient-to-r from-indigo-500 via-fuchsia-500 to-rose-500 dark:from-indigo-400 dark:via-fuchsia-400 dark:to-rose-400">
             affordable
           </span>{" "}
-          and{" "}
-          <span className="text-transparent bg-clip-text bg-gradient-to-r dark:bg-gradient-to-r from-rose-500 via-fuchsia-500 to-indigo-500 dark:from-rose-400 dark:via-fuchsia-400 dark:to-indigo-400">
+            and{" "}
+            <span className="text-transparent bg-clip-text bg-gradient-to-r dark:bg-gradient-to-r from-rose-500 via-fuchsia-500 to-indigo-500 dark:from-rose-400 dark:via-fuchsia-400 dark:to-indigo-400">
             friendly
           </span>{" "}
-          pricing.
-        </h1>
-       
-        <div className="grid grid-cols-1 gap-4 md:grid-cols-3">
-          <div className="p-5 mt-5 mb-10 space-y-6 border shadow-md rounded-2xl">
-            <h1 className="text-xl font-bold text-center text-teal-600 dark:text-teal-300 md:text-2xl sm:text-left">
-              Basic
-            </h1>
-            
-            <div className="mt-8">
+            pricing.
+          </h1>
+
+          <div className="grid grid-cols-1 gap-4 md:grid-cols-3">
+            <div className="p-5 mt-5 mb-10 space-y-6 border border-indigo-500 shadow-md rounded-2xl">
+              <h1 className="text-xl font-bold text-center text-indigo-600 dark:text-indigo-300 md:text-2xl sm:text-left">
+                Basic
+              </h1>
+
+              <div className="mt-8">
               <span className="text-4xl font-extrabold text-gray-900 dark:text-gray-100">
                 $10
               </span>
-              <span className="text-base font-medium text-gray-500">/hour</span>
-              <a
-                href="#"
-                className="block w-full py-2 mt-8 text-sm font-semibold text-center text-gray-300 transition duration-300 bg-black rounded-md hover:bg-gray-800 dark:hover:bg-gray-600 dark:text-gray-200 dark:bg-gray-800"
-              >
-               Get Started
-              </a>
+                <span className="text-base font-medium text-gray-500">/hour</span>
+                <a
+                    href="#"
+                    className="block w-full py-2 mt-8 text-sm font-semibold text-center text-gray-300 transition duration-300 bg-black rounded-md hover:bg-gray-800 dark:hover:bg-gray-600 dark:text-gray-200 dark:bg-gray-800"
+                >
+                  Get Started
+                </a>
+              </div>
+              <div className="w-full border border-gray-300 dark:border-gray-500" />
+              <h1 className="text-sm font-semibold text-gray-500 uppercase">
+                What&apos;s included
+              </h1>
+              <div className="font-normal text-gray-500 dark:text-gray-400 text-md">
+                <ul>
+                  <Feature description="Pay as you go, charges by the hour" />
+                  <Feature description="Access to all the features you love" />
+                </ul>
+              </div>
             </div>
-            <div className="w-full border border-gray-300 dark:border-gray-500" />
-            <h1 className="text-sm font-semibold text-gray-500 uppercase">
-              What&apos;s included
-            </h1>
-            <div className="font-normal text-gray-500 dark:text-gray-400 text-md">
-              <ul>
-                <Feature />
-                <Feature />
-              </ul>
-            </div>
-          </div>
-          <div className="p-5 mb-10 space-y-6 border-2 dark:border-rose-300 rounded-2xl">
-            <h1 className="text-xl font-bold text-center text-teal-600 dark:text-teal-300 md:text-2xl sm:text-left">
-            Premium 
-            </h1>
-            <p className="text-gray-600 text-md dark:text-gray-400">
-              
-            </p>
-            <div className="mt-8">
+            <div className="p-5 mb-10 space-y-6 border-2 border-rose-300 dark:border-rose-300 rounded-2xl">
+              <h1 className="text-xl font-bold text-center text-rose-600 dark:text-rose-300 md:text-2xl sm:text-left">
+                Premium
+              </h1>
+              <p className="text-gray-600 text-md dark:text-gray-400">
+                Most robust package, recomended by geeks!
+              </p>
+              <div className="mt-8">
               <span className="text-4xl font-extrabold text-gray-900 dark:text-gray-100">
-                $70
+                $350
               </span>
-              <span className="text-base font-medium text-gray-500">/day</span>
-              <a
-                href="#"
-                className="block w-full py-2 mt-8 text-sm font-semibold text-center text-gray-300 transition duration-300 bg-black rounded-md hover:bg-gray-800 dark:hover:bg-gray-600 dark:text-gray-200 dark:bg-gray-800"
-              >
-               Get Started
-              </a>
+                <span className="text-base font-medium text-gray-500">/month</span>
+                <a
+                    href="#"
+                    className="block w-full py-2 mt-8 text-sm font-semibold text-center text-gray-300 transition duration-300 bg-black rounded-md hover:bg-gray-800 dark:hover:bg-gray-600 dark:text-gray-200 dark:bg-gray-800"
+                >
+                  Get Started
+                </a>
+              </div>
+              <div className="w-full border border-gray-300 dark:border-gray-500" />
+              <h1 className="text-sm font-semibold text-gray-500 uppercase">
+                What&apos;s included
+              </h1>
+              <div className="font-normal text-gray-500 dark:text-gray-400 text-md">
+                <ul>
+                  <Feature description="Our Largest package" />
+                  <Feature description="VIP treatment, book rooms completely for your geek group" />
+                  <Feature description="60 hours of worry free work" />
+                </ul>
+              </div>
             </div>
-            <div className="w-full border border-gray-300 dark:border-gray-500" />
-            <h1 className="text-sm font-semibold text-gray-500 uppercase">
-              What&apos;s included
-            </h1>
-            <div className="font-normal text-gray-500 dark:text-gray-400 text-md">
-              <ul>
-                <Feature />
-                <Feature />
-                <Feature />
-                <Feature />
-              </ul>
-            </div>
-          </div>
-          <div className="p-5 mt-5 mb-10 space-y-6 border shadow-md rounded-2xl">
-            <h1 className="text-xl font-bold text-center text-teal-600 dark:text-teal-300 md:text-2xl sm:text-left">
-              Subsciber
-            </h1>
-            
-            <div className="mt-8">
+            <div className="p-5 mt-5 mb-10 space-y-6 border border-indigo-500 shadow-md rounded-2xl">
+              <h1 className="text-xl font-bold text-center text-indigo-600 dark:text-indigo-300 md:text-2xl sm:text-left">
+                Subscriber
+              </h1>
+
+              <div className="mt-8">
               <span className="text-4xl font-extrabold text-gray-900 dark:text-gray-100">
-                $100
+                $200
               </span>
-              <span className="text-base font-medium text-gray-500">/mo</span>
-              <a
-                href="#"
-                className="block w-full py-2 mt-8 text-sm font-semibold text-center text-gray-300 transition duration-300 bg-black rounded-md hover:bg-gray-800 dark:hover:bg-gray-600 dark:text-gray-200 dark:bg-gray-800"
-              >
-              Get Started
-              </a>
-            </div>
-            <div className="w-full border border-gray-300 dark:border-gray-500" />
-            <h1 className="text-sm font-semibold text-gray-500 uppercase">
-              What&apos;s included
-            </h1>
-            <div className="font-normal text-gray-500 dark:text-gray-400 text-md">
-              <ul>
-                <Feature />
-                <Feature />
-                <Feature />
-                <Feature />
-                <Feature />
-                <Feature />
-              </ul>
+                <span className="text-base font-medium text-gray-500">/month</span>
+                <a
+                    href="#"
+                    className="block w-full py-2 mt-8 text-sm font-semibold text-center text-gray-300 transition duration-300 bg-black rounded-md hover:bg-gray-800 dark:hover:bg-gray-600 dark:text-gray-200 dark:bg-gray-800"
+                >
+                  Get Started
+                </a>
+              </div>
+              <div className="w-full border border-gray-300 dark:border-gray-500" />
+              <h1 className="text-sm font-semibold text-gray-500 uppercase">
+                What&apos;s included
+              </h1>
+              <div className="font-normal text-gray-500 dark:text-gray-400 text-md">
+                <ul>
+                  <Feature description="The smaller subscription package" />
+                  <Feature description="Fit for geeks working on week-long projects" />
+                  <Feature description="30 hours, don't worry about payment as you leave" />
+
+                </ul>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
   );
 }
