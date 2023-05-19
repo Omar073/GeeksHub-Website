ced2d0fc2b3e78664302411891ed4b6a85a51470
“youssef123tt” <125928226+youssef123tt@users.noreply.github.com>
2023-05-19 22:41:47 +0300

    layout changes
    made the  layout rounded and padding

--- Geeks code with firebase/geeks/src/NewUser.js
+++ Geeks code with firebase/geeks/src/NewUser.js
@@ -136,10 +136,10 @@ const tohome = () =>{  navigate('/'); }
   return (
 
     <div className
-    ="h-screen md:flex  py-5 px-5 rounded-full" >
+    ="h-screen md:flex  py-2 px-2 rounded-full" >
 	<div
 		className
-    ="relative overflow-hidden bg-[#6f596e] bg-blue-500 hover:bg-[#429D9B] md:flex w-1/2 i justify-around items-center hidden">
+    ="rounded-md relative overflow-hidden bg-[#6f596e] bg-blue-500 hover:bg-[#429D9B] md:flex w-1/2 i justify-around items-center hidden">
 		<div>
       {/* geeks hub label */}
 			<h1 className
@@ -160,7 +160,7 @@ const tohome = () =>{  navigate('/'); }
     ="absolute -top-20 -right-20 w-80 h-80 border-4 rounded-full border-opacity-30 border-t-8"></div>
 	</div>
   {/* container */}
-  <div class="container mx-auto">
+
   
 	<div className
   ="flex md:w-1/2 justify-center py-10 items-center  ">
@@ -180,7 +180,7 @@ const tohome = () =>{  navigate('/'); }
 						clip-rule="evenodd" />
 				</svg>
 				<input className
-        ="pl-2 outline-none border-none"  onChange={(e) => setFirstName(e.target.value)} type="text" name="" id="" placeholder="Full name" />
+        =" pl-2 outline-none border-none"  onChange={(e) => setFirstName(e.target.value)} type="text" name="" id="" placeholder="Full name" />
       </div>
 {/* last name */}
 				<div className
@@ -192,7 +192,7 @@ const tohome = () =>{  navigate('/'); }
 							d="M12 11c0 3.517-1.009 6.799-2.753 9.571m-3.44-2.04l.054-.09A13.916 13.916 0 008 11a4 4 0 118 0c0 1.017-.07 2.019-.203 3m-2.118 6.844A21.88 21.88 0 0015.171 17m3.839 1.132c.645-2.266.99-4.659.99-7.132A8 8 0 008 4.07M3 15.364c.64-1.319 1-2.8 1-4.364 0-1.457.39-2.823 1.07-4" />
 					</svg>
 					<input className
-          ="pl-2 outline-none border-none" type="text" name="" id="" placeholder="Last Name"  onChange={(e) => setLastName(e.target.value)} />
+          ="  pl-2 outline-none border-none" type="text" name="" id="" placeholder="Last Name"  onChange={(e) => setLastName(e.target.value)} />
       </div>
 {/* email */}
 					<div className
@@ -204,7 +204,7 @@ const tohome = () =>{  navigate('/'); }
 								d="M16 12a4 4 0 10-8 0 4 4 0 008 0zm0 0v1.5a2.5 2.5 0 005 0V12a9 9 0 10-9 9m4.5-1.206a8.959 8.959 0 01-4.5 1.207" />
 						</svg>
 						<input className
-            ="pl-2 outline-none border-none" type="text" name="" id="" placeholder="Email Address" onChange={(e)=>setEmail(e.target.value)} />
+            ="  pl-2 outline-none border-none" type="text" name="" id="" placeholder="Email Address" onChange={(e)=>setEmail(e.target.value)} />
       </div>
 {/* password */}
 						<div className
@@ -217,7 +217,7 @@ const tohome = () =>{  navigate('/'); }
 									clip-rule="evenodd" />
 							</svg>
 							<input className
-              ="pl-2 outline-none border-none" type="text" name="" id="" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+              ="  pl-2 outline-none border-none" type="text" name="" id="" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
       </div>
       {/* confirm password */}
       <div className
@@ -230,7 +230,7 @@ const tohome = () =>{  navigate('/'); }
 									clip-rule="evenodd" />
 							</svg>
 							<input className
-              ="pl-2 outline-none border-none" type="text" name="" id="" placeholder="confirm password"  />
+              ="  pl-2 outline-none border-none" type="text" name="" id="" placeholder="confirm password"  />
       </div>
 {/* phone number*/}
 <div className
@@ -243,7 +243,7 @@ const tohome = () =>{  navigate('/'); }
 									clip-rule="evenodd" />
 							</svg>
 							<input className
-              ="pl-2 outline-none border-none" type="text" name="" id="" placeholder="phone number" onChange={(e) => setPhoneNumber(e.target.value)} />
+              =" pl-2 outline-none border-none" type="text" name="" id="" placeholder="phone number" onChange={(e) => setPhoneNumber(e.target.value)} />
       </div>
      {/* your faculty */}
       <div className
@@ -256,7 +256,7 @@ const tohome = () =>{  navigate('/'); }
 									clip-rule="evenodd" />
 							</svg>
 							<input className
-              ="pl-2 outline-none border-none" type="text" name="" id="" placeholder="your faculty" onChange={(e) => setFaculty(e.target.value)} />
+              =" bg-[#429D9B]pl-2 outline-none border-none" type="text" name="" id="" placeholder="your faculty" onChange={(e) => setFaculty(e.target.value)} />
       </div>
 							<button type="submit" className
               ="block w-full bg-indigo-600 mt-4 py-2 rounded-2xl text-white font-semibold mb-2" onClick={handleSubmit} >sign up</button>
@@ -266,6 +266,6 @@ const tohome = () =>{  navigate('/'); }
     </div>
 	</div>
   
-</div>
+
   )
 }
