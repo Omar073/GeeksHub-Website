645af7e0c889fcd99f52684f650c78a394fd6101
“youssef123tt” <125928226+youssef123tt@users.noreply.github.com>
2023-05-19 20:05:32 +0300

    sign up new version

--- THE Project Files/final-version/geeks-v-3/pages/signup.js
+++ THE Project Files/final-version/geeks-v-3/pages/signup.js
@@ -21,7 +21,7 @@ export default function signup() {
           cardType: "summary_large_image",
         }}
       />
-    <section className="bg-gray-50 dark:bg-gray-900">
+   
   <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
       <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
       <img className="w-20 h-10 mr-2" src='/images/logo geeks.png' alt="logo" 
@@ -31,7 +31,7 @@ export default function signup() {
       <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
               <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                  Create and account
+                  Create and account 
               </h1>
               <form className="space-y-4 md:space-y-6" action="#">
                   <div>
