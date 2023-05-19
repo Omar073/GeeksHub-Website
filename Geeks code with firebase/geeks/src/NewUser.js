578510d10fd23ab531d55bdec5382f22d5697f2b
“youssef123tt” <125928226+youssef123tt@users.noreply.github.com>
2023-05-19 22:54:32 +0300

    change  colors of left side

--- Geeks code with firebase/geeks/src/NewUser.js
+++ Geeks code with firebase/geeks/src/NewUser.js
@@ -139,7 +139,7 @@ const tohome = () =>{  navigate('/'); }
     ="h-screen md:flex  py-2 px-2 shadow-2xl rounded-full" >
 	<div
 		className
-    ="rounded-md relative overflow-hidden bg-[#6f596e] bg-blue-500 hover:bg-[#429D9B] md:flex w-1/2 i justify-around items-center hidden">
+    ="rounded-md relative overflow-hidden bg-gradient-to-tr from-blue-800 to-purple-700 md:flex w-1/2 i justify-around items-center hidden ">
 		<div>
       {/* geeks hub label */}
 			<h1 className
