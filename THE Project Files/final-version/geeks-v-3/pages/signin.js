ed082759ae8a47f3e6a66e41e67ea4f915248ace
“youssef123tt” <21P0130@eng.asu.edu.eg>
2023-05-19 18:30:02 +0300

    test

--- THE Project Files/final-version/geeks-v-3/pages/signin.js
+++ THE Project Files/final-version/geeks-v-3/pages/signin.js
@@ -27,6 +27,7 @@ export default function login() {
       <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white justify-items-center">
           <img className="w-20 h-10 mr-2" src='/images/logo geeks.png' alt="logo" 
          />
+         {/* rggrggrgr */}
       </a>
       <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
